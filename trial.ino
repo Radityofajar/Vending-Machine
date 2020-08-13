d60e4444d68cfa64334462562986f4fad6984480
Radityo Fajar <54077977+Radityofajar@users.noreply.github.com>
2020-08-13 11:27:27 +0700

    Update trial.ino

--- trial.ino
+++ trial.ino
@@ -26,30 +26,46 @@ void koordinat(int Horizontal, int Vertikal){
 
 void loop() {
   Serial.println("Run Bersama");
-  koordinat(200,200);
+  koordinat(2000,2000);
   delay(2000);
   Serial.println("Kembali ke posisi semula bersama");
   koordinat(0,0);
   delay(2000);
   Serial.println("Hanya vertikal");
-  koordinat(0,200);
+  koordinat(0,2000);
   delay(2000);
   Serial.println("Vertikal kembali ke posisi semula");
   koordinat(0,0);
   delay(2000);
   Serial.println("Hanya horizontal");
-  koordinat(200,0);
+  koordinat(2000,0);
   delay(2000);
   Serial.println("Horizontal kembali ke posisi semula");
   koordinat(0,0);
   delay(2000);
   Serial.println("Run Bersama");
-  koordinat(200,200);
+  koordinat(2000,2000);
   delay(2000);
   Serial.println("Horizontal bergerak ke posisi semula");
-  koordinat(0,200);
-  delay(2000);
+  koordinat(0,2000);
+  delay(20000);
   Serial.println("Vertikal bergerak ke posisi semula");
   koordinat(0,0);
   delay(2000);
+  Serial.println("Run dan stop bersama dengan jarak berbeda");
+  koordinat(1000,2000);
+  delay(2000);
+  Serial.println("Kembali ke posisi semula");
+  koordinat(0,0);
+  delay(2000);
+  Serial.println("Run dan stop bersama dengan jarak berbeda");
+  koordinat(2000,1000);
+  delay(2000);
+  Serial.println("Run dan stop bersama dengan jarak berbeda");
+  koordinat(3250, 2276);
+  delay(2000);
+  Serial.println("Kembali ke posisi semula");
+  koordinat(0,0);
+  delay(500);
+  Serial.println("Selesai")
 }
