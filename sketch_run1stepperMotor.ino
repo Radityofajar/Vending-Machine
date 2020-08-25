eb78466402ebeb71df7f7745a937f610e4faf764
andreapramadityaperwiraputra <69444409+andreapramadityaperwiraputra@users.noreply.github.com>
2020-08-25 15:56:51 +0700

    Update sketch_run1stepperMotor.ino

--- sketch_run1stepperMotor.ino
+++ sketch_run1stepperMotor.ino
@@ -1,95 +1,51 @@
 //control 1 stepper motor using A4988 driver module
 
-#define dirPin 0
-#define stepPin 1
-
-#define microSwitch 2
-
-#define button1 3
-#define button2 4
-#define button3 5
-
-int buttonPressed;
+#define dirPin 2
+#define stepPin 3
 
 void setup(){
   pinMode(dirPin, OUTPUT);
   pinMode(stepPin, OUTPUT);
-
-  pinMode(button1, INPUT_PULLUP);
-  pinMode(button2, INPUT_PULLUP);
-  pinMode(button3, INPUT_PULLUP);
-
-  pinMode(microSwitch, INPUT_PULLUP);
-
-  //kalibrasi posisi awal kontainer
-  digitalWrite(dirPin, HIGH);
-  while(true){
-    if(digitalRead(microSwitch)==LOW){
-      moveUp(70);
-      break;
-    }
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(300);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(300);
-  }
 }
 
 //function moveUp
 void moveUp(int steps){
-  digitalWrite(dirPin, LOW);
+  digitalWrite(dirPin, HIGH);
   for(int x = 0; x < steps; x++){
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(300);
+    delayMicroseconds(1000);
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(300);
+    delayMicroseconds(1000);
   }
 }
 
 //function moveDown
 void moveDown(int steps){
-  digitalWrite(dirPin, HIGH);
+  digitalWrite(dirPin, LOW);
   for(int x = 0; x < steps; x++){
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(300);
+    delayMicroseconds(1000);
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(300);
+    delayMicroseconds(1000);
   }
 }
 
 void loop(){
   while(true){
-  if(digitalRead(button1)==LOW){
-    buttonPressed = 1;
-    //break;
-  }
-  if(digitalRead(button2)==LOW){
-    buttonPressed = 2;
-    //break;
-  }
-  if(digitalRead(button3)==LOW){
-    buttonPressed = 3;
-    //break;
-  }
-
-  switch(buttonPressed){
-    case 1:
-    moveUp(200);
-    delay(200);
-    moveDown(200);
-    break;
-
-    case 2:
+  
     moveUp(400);
-    delay(200);
+    delay(500);
     moveDown(400);
-    break;
+    delay(500);
 
-    case 3:
-    moveUp(600);
-    delay(200);
-    moveDown(600);
-    break;
-  }
+    moveUp(800);
+    delay(500);
+    moveDown(800);
+    delay(500);
+    
+    moveUp(1000);
+    delay(500);
+    moveDown(1000);
+    delay(500);
   }
 }
